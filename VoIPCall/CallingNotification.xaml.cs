3d69a4a2d3647b2d7cb4a152e0ee8f820bb36b02
tanloibdp <tanloi.bdp@gmail.com>
2019-07-04 12:32:38 +0700

    Add Project

---
+++ VoIPCall/CallingNotification.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VoIPCall
+{
+    /// <summary>
+    /// Interaction logic for CallingNotification.xaml
+    /// </summary>
+    public partial class CallingNotification : Window
+    {
+        public CallingNotification()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
